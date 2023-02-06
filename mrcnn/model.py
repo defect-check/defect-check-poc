aad3b2ec6ae029184a9527ade9fef7f0134fefdc
Rowend Duke <rowendduke36@gmail.com>
2023-02-06 12:46:03 +0100

    feat: change name of log dir to checkpoints

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -3203,7 +3203,7 @@ class MaskRCNNRPN():
         # Directory for training logs
 #        self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(
 #            self.config.NAME.lower(), now))
-        self.log_dir = "D:\\Object Detection\\Kangaro - Transfer Learning\\logdir\\train"
+        self.log_dir = "checkpoints"
 
         # Path to save after each epoch. Include placeholders that get filled by Keras.
         self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
