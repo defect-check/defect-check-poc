b47f62011729d2563f7531b0ad149e3e9969f612
ROWEND36 <rowendduke36@gmail.com>
2023-04-06 10:20:35 +0100

    switched to pytorch

---
+++ src/run_model.py
@@ -0,0 +1,6 @@
+def run_model(model, image_path):
+    """
+    This function is in charge or running the defect detect model in application mode
+    It accepts an image path as input an returns the location of sagging conductors in
+    the image if any.
+    """
