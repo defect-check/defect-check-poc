19407ca6056878c168f2512366a846eee62eb6fa
ROWEND36 <rowendduke36@gmail.com>
2023-04-19 15:51:05 +0100

    fix: bug in run_model

--- src/run_model.py
+++ src/run_model.py
@@ -9,20 +9,29 @@ def run_model(model, image_path):
     It accepts an image path as input an returns the location of sagging conductors in
     the image if any.
     """
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
     img, _ = get_transform(False)(Image.open(image_path).convert("RGB"), {"boxes": []})
     model = model.eval()
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     with torch.no_grad():
         prediction = model([img.to(device)])
 
-    mask = Image.fromarray(
-        255 - prediction[0]["masks"][3, 0].mul(255).byte().cpu().numpy(), "L"
-    )
     target = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy(), "RGB")
 
     red = Image.new("RGB", target.size, (255, 0, 0))
+    green = Image.new("RGB", target.size, (0, 255, 0))
 
-    # create a mask using RGBA to define an alpha channel to make the overlay transparent
-    # mask = Image.new('RGBA',target.size,(0,0,0,123))
+    for i in range(len(prediction[0]["labels"])):
+        mask = Image.fromarray(
+            255
+            - prediction[0]["masks"][i, 0]
+            .mul(255 * prediction[0]["scores"][i])
+            .byte()
+            .cpu()
+            .numpy(),
+            "L",
+        )
+        color = red if prediction[0]["labels"][i] == 1 else green
+        target = Image.composite(target, color, mask)
 
-    return Image.composite(target, red, mask)
+    return target
