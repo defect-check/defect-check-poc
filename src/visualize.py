4346224d64b7320f5adfcaacfcefb1ef9fda0603
ROWEND36 <rowendduke36@gmail.com>
2023-05-03 19:53:24 +0100

    fix: bug in compose

--- src/visualize.py
+++ src/visualize.py
@@ -15,15 +15,28 @@ from .config import PALETTE
 from . import utils
 
 
-def compose_masks(target, data, use_scores=False, palette=PALETTE, cache={}):
+def compose_masks_predicted(target, data, palette=PALETTE, cache={}):
     result = target
     for i in range(len(data["labels"])):
-        score = data["scores"][i] if use_scores else 1
+        score = data["scores"][i]
         label = data["labels"][i]
         if score < 0.2:
             continue
         mask = Image.fromarray(
             255 - data["masks"][i, 0].mul(255 * score).byte().cpu().numpy(), "L"
+        )
+
+        if label not in cache:
+            cache[label] = Image.new("RGB", target.size, PALETTE[int(label)])
+        result = Image.composite(result, cache[label], mask)
+    return result
+
+def compose_masks_expected(target, data, palette=PALETTE, cache={}):
+    result = target
+    for i in range(len(data["labels"])):
+        label = data["labels"][i]
+        mask = Image.fromarray(
+            255 - data["masks"][i].mul(255).byte().cpu().numpy(), "L"
         )
 
         if label not in cache:
